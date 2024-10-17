1a4099ac586623dedb7498ded07b371ef18a87c9
kenntcky <hologrampulsion@gmail.com>
2024-10-17 21:36:02 +0700

    [+] Finish SQLite data storing logic

--- lib/database.dart
+++ lib/database.dart
@@ -1,79 +1,87 @@
-// lib/database_helper.dart
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+  // lib/database_helper.dart
+  import 'package:sqflite/sqflite.dart';
+  import 'package:path/path.dart';
+  import 'package:shared_preferences/shared_preferences.dart';
 
-class DatabaseHelper {
-  static final DatabaseHelper _instance = DatabaseHelper._internal();
-  factory DatabaseHelper() => _instance;
+  class DatabaseHelper {
+    static final DatabaseHelper _instance = DatabaseHelper._internal();
+    factory DatabaseHelper() => _instance;
 
-  static Database? _database;
+    static Database? _database;
 
-  DatabaseHelper._internal();
+    DatabaseHelper._internal();
 
-  Future<Database> get database async {
-    if (_database != null) return _database!;
+    Future<Database> get database async {
+      if (_database != null) return _database!;
 
-    print("Hello");
+      print("Hello");
 
-    _database = await _initDatabase();
-    return _database!;
+      _database = await initDatabase();
+      return _database!;
+    }
+
+    Future<Database> initDatabase() async {
+      final prefs = await SharedPreferences.getInstance();
+      bool isDatabaseInitialized = prefs.getBool('dbInitialized') ?? false;
+      String path = join(await getDatabasesPath(), 'dataGempa.db');
+
+      if (!isDatabaseInitialized){
+        await prefs.setBool('dbInitialized', true);
+        return await openDatabase(
+        path,
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute('''
+            CREATE TABLE dataGempa(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              tanggal TEXT,
+              jam TEXT,
+              coordinates TEXT,
+              lintang TEXT,
+              bujur TEXT,
+              magnitude TEXT,
+              kedalaman TEXT,
+              wilayah TEXT,
+              potensi TEXT,
+              dirasakan TEXT,
+              shakemap BLOB,
+              tsunamiPotensial INTEGER,
+              kabupaten TEXT
+            )
+          ''');
+        },
+      );
+      } else {
+        return await openDatabase(path);
+      }
+    }
+
+    Future<List<Map<String, dynamic>>> getLatestGempa() async {
+      final db = await database;
+
+      print("Hello");
+
+      // Query to get the latest entry from the gempa table, ordered by id in descending order
+      return await db.query(
+        'dataGempa',
+        orderBy: 'id DESC', // Order by id descending (most recent first)
+        limit: 1, // Get only the latest entry
+      );
+    }
+
+    Future<void> insertGempa(Map<String, dynamic> gempaData) async {
+      final db = await database;
+      await db.insert('dataGempa', gempaData,
+          conflictAlgorithm: ConflictAlgorithm.replace);
+    }
+
+    Future<List<Map<String, dynamic>>> getAllGempa() async {
+      final db = await database;
+      return await db.query('dataGempa');
+    }
+
+    Future<void> deleteAll() async {
+      final db = await database;
+      await db.delete('dataGempa');
+    }
   }
-
-  Future<Database> _initDatabase() async {
-    String path = join(await getDatabasesPath(), 'gempa.db');
-
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: (db, version) async {
-        await db.execute('''
-          CREATE TABLE gempa(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tanggal TEXT,
-            jam TEXT,
-            lintang TEXT,
-            bujur TEXT,
-            magnitude TEXT,
-            kedalaman TEXT,
-            coordinates TEXT,
-            status TEXT,
-            wilayah TEXT,
-            jenisGempa TEXT,
-            tsunamiPotensial TEXT,
-            infoTambahan TEXT,
-            kabupaten TEXT
-          )
-        ''');
-      },
-    );
-  }
-
-  Future<List<Map<String, dynamic>>> getLatestGempa() async {
-    final db = await database;
-
-    print("Hello");
-
-    // Query to get the latest entry from the gempa table, ordered by id in descending order
-    return await db.query(
-      'gempa',
-      orderBy: 'id DESC', // Order by id descending (most recent first)
-      limit: 1, // Get only the latest entry
-    );
-  }
-
-  Future<void> insertGempa(Map<String, dynamic> gempaData) async {
-    final db = await database;
-    await db.insert('gempa', gempaData,
-        conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-
-  Future<List<Map<String, dynamic>>> getAllGempa() async {
-    final db = await database;
-    return await db.query('gempa');
-  }
-
-  Future<void> deleteAll() async {
-    final db = await database;
-    await db.delete('gempa');
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
@@ -5,6 +7,11 @@ import 'package:workmanager/workmanager.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'database.dart';
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
 
 // WorkManager callback dispatcher
 @pragma(
@@ -22,35 +29,23 @@ void callbackDispatcher() {
       print('Firebase already initialized: $e');
     }
 
-    // Open SQLite database
-    Database db = await _openDatabase();
-
-    // Fetch data from SQLite
-    List<Map<String, dynamic>> localData = await db.query('earthquake_data');
-    if (localData.isNotEmpty) {
-      // Sync with Firebase if there is data
-      DatabaseReference ref = FirebaseDatabase.instance.ref();
-
-      for (var data in localData) {
-        await ref.push().set({
-          'Wilayah': data['wilayah'],
-          'Coordinates': data['coordinates'],
-        });
-      }
-
-      print("Data synced with Firebase");
-    } else {
-      print("No data to sync");
-    }
+    readData();
 
     return Future.value(true);
   });
 }
 
+// Function to download the image and convert it to bytes
+Future<Uint8List> downloadImageAsBytes(String imageUrl) async {
+  final response = await http.get(Uri.parse(imageUrl));
+  return response.bodyBytes; // This gives you the image data in bytes
+}
+
+
 void readData() async {
   DatabaseReference ref = FirebaseDatabase.instance.ref();
 
-  ref.orderByKey().limitToLast(1).onValue.listen((DatabaseEvent event) {
+  ref.orderByKey().limitToLast(1).onValue.listen((DatabaseEvent event) async {
     final data = event.snapshot.value;
     if (data != null) {
       Map<String, dynamic> dataMap = Map<String, dynamic>.from(data as Map);
@@ -59,13 +54,24 @@ void readData() async {
       Map<String, dynamic> latestEarthquake =
           Map<String, dynamic>.from(dataMap[latestKey]['Infogempa']['gempa']);
 
-      print(latestEarthquake["Wilayah"]);
-      print(latestEarthquake["Coordinates"]);
+      print("DATA GEMPA TERBARU:");
+      print(latestEarthquake["shakemapUrl"]);
+
+      inspect(downloadImageAsBytes(latestEarthquake["shakemapUrl"]));
 
       // Save data locally to SQLite
       _saveToLocalDatabase(
-        wilayah: latestEarthquake["Wilayah"],
+        tanggal: latestEarthquake["Tanggal"],
+        jam: latestEarthquake["Jam"],
         coordinates: latestEarthquake["Coordinates"],
+        lintang: latestEarthquake["Lintang"],
+        bujur: latestEarthquake["Bujur"],
+        magnitude: latestEarthquake["Magnitude"],
+        kedalaman: latestEarthquake["Kedalaman"],
+        wilayah: latestEarthquake["Wilayah"],
+        potensi: latestEarthquake["Potensi"],
+        dirasakan: latestEarthquake["Dirasakan"],
+        shakemap: await downloadImageAsBytes(latestEarthquake["shakemapUrl"])
       );
     }
   });
@@ -74,20 +80,11 @@ void readData() async {
 // Function to open and create SQLite database
 Future<Database> _openDatabase() async {
   String databasesPath = await getDatabasesPath();
-  String dbPath = join(databasesPath, 'earthquake_data.db');
+  String dbPath = join(databasesPath, 'dataGempa.db');
 
   Database db = await openDatabase(
     dbPath,
-    version: 1,
-    onCreate: (Database db, int version) async {
-      await db.execute('''
-        CREATE TABLE earthquake_data (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          wilayah TEXT,
-          coordinates TEXT
-        )
-      ''');
-    },
+    version: 1
   );
 
   return db;
@@ -95,26 +92,45 @@ Future<Database> _openDatabase() async {
 
 // Function to save data into SQLite database
 Future<void> _saveToLocalDatabase(
-    {required String wilayah, required String coordinates}) async {
+    {required String tanggal, 
+    required String jam, 
+    required String coordinates, 
+    required String lintang, 
+    required String bujur, 
+    required String magnitude, 
+    required String kedalaman, 
+    required String wilayah, 
+    required String potensi, 
+    required String dirasakan, 
+    required Uint8List shakemap}) async {
   Database db = await _openDatabase();
 
   // Check the number of existing entries
-  List<Map<String, dynamic>> existingData = await db.query('earthquake_data');
+  List<Map<String, dynamic>> existingData = await db.query('dataGempa');
 
   // If there are 10 or more entries, delete the oldest
   if (existingData.length >= 10) {
     // Remove the oldest entry
     await db.delete(
-      'earthquake_data',
+      'dataGempa',
       where: 'id = ?',
       whereArgs: [existingData.first['id']],
     );
   }
 
   // Insert new data into SQLite
-  await db.insert('earthquake_data', {
-    'wilayah': wilayah,
+  await db.insert('dataGempa', {
+    'tanggal': tanggal,
+    'jam': jam,
     'coordinates': coordinates,
+    'lintang': lintang,
+    'bujur': bujur,
+    'magnitude': magnitude,
+    'kedalaman': kedalaman,
+    'wilayah': wilayah,
+    'potensi': potensi,
+    'dirasakan': dirasakan,
+    'shakemap': shakemap
   });
 
   print("Data saved to SQLite");
@@ -122,6 +138,8 @@ Future<void> _saveToLocalDatabase(
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseHelper().initDatabase();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -131,13 +149,14 @@ void main() async {
 
   // Initialize WorkManager
   readData();
-  print("main func");
   Workmanager().initialize(
       callbackDispatcher, // The top level function, aka callbackDispatcher
       isInDebugMode:
           false // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
       );
   Workmanager().registerOneOffTask("task-identifier", "simpleTask");
+
+  print(await DatabaseHelper().getAllGempa());
 
   runApp(
     GetMaterialApp(
